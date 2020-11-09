18108a7f8e2999f2f49ee63002ecf0e9c7d8317f
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-09 11:27:22 +0900

    add condition to copy always if overlapped

--- Libft/00_libc/ft_memmove.c
+++ Libft/00_libc/ft_memmove.c
@@ -6,16 +6,22 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 10:47:25 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/06 10:54:19 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/09 11:25:55 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char d;
+	unsigned char *d;
+	unsigned char *s;
 
 	d = (unsigned char *)dst;
-	while (len-- > 0)
-		*d++ = *src++;
+	s = (unsigned char *)src;
+	if (s < d && s + len > d || d < s && d + len > s)
+		while (len-- > 0)
+			*d + len = *s + len;
+	else
+		while (len-- > 0)
+			*d++ = *s++;
 	return (dst);
 }
