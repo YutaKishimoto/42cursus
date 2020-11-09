965e47f1e2e0e83c01ff9992f21d6c242311c212
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-09 12:03:43 +0900

    fix some mem-funcs and add isdigit

--- Libft/00_libc/ft_isdigit.c
+++ Libft/00_libc/ft_isdigit.c
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   _ft_memmove.c                                      :+:      :+:    :+:   */
+/*   _ft_isdigit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/06 10:47:25 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/09 11:54:22 by tyamagis         ###   ########.fr       */
+/*   Created: 2020/11/09 11:55:18 by tyamagis          #+#    #+#             */
+/*   Updated: 2020/11/09 12:02:13 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+int	ft_isdigit(int c)
 {
-	char		*d;
-	const char	*s;
-
-	d = (char *)dst;
-	s = (const char *)src;
-	if ((s < d && s + len > d) || (d < s && d + len > s))
-	{
-		while (len-- > 0)
-			*(d + len) = *(s + len);
-	}
-	else
-	{
-		while (len-- > 0)
-			*d++ = *s++;
-	}
-	return (dst);
+	return ('0' <= c && c <= '9');
 }
--- Libft/00_libc/ft_memset.c
+++ Libft/00_libc/ft_memset.c
@@ -6,18 +6,18 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 08:28:45 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/09 11:45:37 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/09 11:54:08 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	uc;
-	void			*tmp;
+	unsigned char	*tmp;
 
 	uc = c;
-	tmp = b;
+	tmp = (unsigned char *)b;
 	while (len-- > 0)
-		*(unsigned char *)tmp++ = uc;
+		*tmp++ = uc;
 	return (b);
 }
