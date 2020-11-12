dd533351ccef5925728d926ada86e267a4efe20a
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-12 22:37:29 +0900

    mod ft_atoi.c

--- Libft/00_libc/_ft_atoi.c
+++ Libft/00_libc/_ft_atoi.c
@@ -6,13 +6,20 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 21:42:36 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/12 22:08:42 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/12 22:36:44 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	long l;
+	long	l;
+	int		minus;
 
 	if (ft_isspace(*str))
 		str++;
+	if (*str++ == '-')
+		minus = -1;
+	while (ft_isdigit(*str))
+	{
+	}
+	
