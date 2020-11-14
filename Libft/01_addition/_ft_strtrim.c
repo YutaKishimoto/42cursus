d0c8f6495658a88fdcae710f77ee5b20d30a208b
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-14 21:18:26 +0900

    add _ft_strtrim.c (create later...)

---
+++ Libft/01_addition/_ft_strtrim.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/14 20:52:09 by tyamagis          #+#    #+#             */
+/*   Updated: 2020/11/14 21:11:06 by tyamagis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	
+
+	s1[0] ?=  set[0] - set [j] == \0
+	TRUE >> s1[1] ?= ...
+		FALSE >> s1[i] ?= ...
+			TRUE >> s1[i - 1] ?= ...
+		trim = s1[i]_s[j]
+	return (trim)
