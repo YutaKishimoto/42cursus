0120ee6fc52d45f6cfdef36ef7801b49d2ab7ab9
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-14 19:47:56 +0900

    add ft_substr.c

---
+++ Libft/01_addition/ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/14 15:15:18 by tyamagis          #+#    #+#             */
+/*   Updated: 2020/11/14 19:46:32 by tyamagis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*ss;
+
+	if (!(ss = (char *)malloc(len + 1)))
+		return (NULL);
+	i = 0;
+	while (len - i > 0)
+	{
+		*(ss + i) = *(s + start + i);
+		i++;
+	}
+	*ss = '\0';
+	return (ss);
+}
