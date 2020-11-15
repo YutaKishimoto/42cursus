d5e2ab5a30e3fe114110056f90c3b657803957e7
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-15 20:04:13 +0900

    add ft_split.c

--- Libft/01_addition/ft_split.c
+++ Libft/01_addition/ft_split.c
@@ -1,32 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/14 20:38:16 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/15 19:45:13 by tyamagis         ###   ########.fr       */
+/*   Created: 2020/11/15 19:45:53 by tyamagis          #+#    #+#             */
+/*   Updated: 2020/11/15 20:04:01 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.c"
 #include <stdlib.h>
-#include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	**ft_split(char const *s, char c)
 {
-	size_t	l1;
-	size_t	l2;
-	char	*s;
+	char	**split;
 
-	l1 = ft_strlen(s1);
-	l2 = ft_strlen(s2);
-	if (!(s = (char *)malloc(l1 + l2 + 1)))
-		return (s);
-	while (*s1)
-		*s++ = *s1++;
-	while (*s2)
-		*s++ = *s2++;
-	*s = '\0';
-	return (s - (l1 + l2 + 1));
+	if (!(split = malloc(ft_strlen(s) + 1)))
+		return (NULL);
+	while (*s)
+	{
+		if (*s != c)
+			**split++ = *s++;
+		else
+		{
+			**split++ = '\0';
+			*split = split;
+		}
+	}
+	**split = (NULL);
+	return (split);
 }
