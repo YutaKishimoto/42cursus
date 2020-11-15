d276026fee43d070a81fee307c8355c7290bb6f0
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-15 21:40:38 +0900

    mod ft_memmove.c

--- Libft/00_libc/ft_memmove.c
+++ Libft/00_libc/ft_memmove.c
@@ -6,9 +6,11 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 10:47:25 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/09 11:54:22 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/15 21:39:16 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -17,7 +19,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	d = (char *)dst;
 	s = (const char *)src;
-	if ((s < d && s + len > d) || (d < s && d + len > s))
+	if (s < d && s + len > d)
 	{
 		while (len-- > 0)
 			*(d + len) = *(s + len);
@@ -28,4 +30,21 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 			*d++ = *s++;
 	}
 	return (dst);
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char dst1[30] = "That's not what i said.";
+	char dst2[30] = "That's not what i said.";
+
+	ft_memmove(dst1 + 7, dst1 + 11, 13);
+	memmove(dst2 + 7, dst2 + 11, 13);
+
+	printf("ft_memmove : \t %s \n", dst1);
+	printf("memmove : \t %s \n", dst2);
+
+	return (0);
 }
