dff22c1cbee568bd5db1895186b8fd6e4bf3ff14
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-19 22:51:50 +0900

    mod linft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 23:43:11 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/19 22:07:33 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/19 22:20:59 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
-int					ft_memcmp(const void *s1, const vpod *s2, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t				ft_strlen(const char *s);
 size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
