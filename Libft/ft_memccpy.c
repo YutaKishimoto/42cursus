b1cd6d4214d41f5f7e4ba63056ca7d9c8b6ca52d
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-21 03:53:25 +0900

    mod ft_memccpy_test (not copmleted

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 09:57:38 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/19 23:12:16 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/21 03:29:36 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,36 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	const unsigned char	*s;
 	unsigned char		uc;
 
-	d = dst;
-	uc = c;
-	while (n-- > 0 && *src != uc)
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	uc = (unsigned char)c;
+	while (n-- > 0 && *s != uc)
 	{
-		if (*src == 0)
+		if (*s == 0)
 			return (NULL);
-		*d++ = *src++;
+		*d++ = *s++;
 	}
 	return (d);
 }
+
+#include <string.h>
+int		main(void)
+{
+	int		c = 'A' - 1;
+	size_t	n = 0;
+
+	while (c <= 'T')
+	{
+		n = 0;
+		while (n < 10)
+		{
+			printf("%d, %zu)\n", c, n);
+			printf("original : \"%s\"\n", memccpy("abcdefg", "ABCDEFG", c, n));
+			printf("ft_memccpy(\"%s\"\n", ft_memccpy("abcdefg", "ABCDEFG", c, n));
+			n++;
+		}
+		c++;
+	}
+	return (0);
+}
+
