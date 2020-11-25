123982f961ce349afa2cc70d43901699106bdb57
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-26 07:17:00 +0900

    mod ft_strmapi

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 21:48:44 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/14 22:01:01 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/26 07:02:55 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	s_len;
-	char	*f_str;
-	int		i;
+	size_t			s_len;
+	char			*f_str;
+	unsigned int	i;
 
 	s_len = ft_strlen(s);
 	if (!(f_str = (char *)malloc(s_len + 1)))
 		return (NULL);
 	i = 0;
 	while (*s)
-	{
-		f_str[i] = f(i, *s);
-		i++;
-	}
-	f_str[i] = '\0';
-	return (f_str);
+		*f_str++ = f(i++, *s++);
+	*f_str = '\0';
+	return (f_str - s_len);
 }
