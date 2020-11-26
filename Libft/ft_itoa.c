212ef3a6bb82325428c73a8d4b6aa0fd47f1d6ee
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-26 22:50:45 +0900

    = ^ v ^ = NYAAAAANNNNN

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,35 +6,78 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 20:06:14 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/23 19:31:30 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/26 22:49:33 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_itoa(int n)
+static char	cnt_index(long l)
 {
-	char	*a;
+	char idx;
 
-	if (!(a = (char *)malloc(12)))
+	idx = 0;
+	while (l < 10)
+	{
+		l /= 10;
+		idx++;
+	}
+	return (idx);
+}
+
+char		*ft_itoa(int n)
+{
+	char	*s;
+	long	l;
+	int		idx;
+	int		minus;
+
+	l = n;
+	if (l < 0)
+	{
+		l *= -1;
+		minus = 1;
+	}
+	idx = cnt_index(l);
+	if (!(s = (char *)malloc(idx + minus + 1)))
 		return (NULL);
-	if (n < 0)
+	*(s + idx + minus) = '\0';
+	while (l != 0)
 	{
-		if (n == -2147483648)
-			return (a = "-2147483648");
-		else
-		{
-			n *= -1;
-			*a = '-';
-		}
+		*--s = '0' + l % 10;
+		l /= 10;
 	}
-	a += 11;
-	*a-- = '\0';
-	while (n != 0)
-	{
-		*a-- = '0' + n % 10;
-		n /= 10;
-	}
-	return (a);
+	if (minus)
+		*--s = '-';
+	return (s);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 1234567890;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 12345678901;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 23456789012;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 21098765432;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = -12345678901;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = -123456789012;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = -234567890123;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = -2147483648;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 2147483647;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	n = 2147483648;
+	printf("n = %d, ft_itoa(%d) is %s\n", n, n, ft_itoa(n));
+	return (0);
 }
--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,36 +6,47 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 19:45:53 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/26 11:47:45 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/26 22:40:19 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	**ft_split(char const *s, char c)
+static char	*ft_subsplit(char *spl, char *t, char c)
+{
+	while (*t)
+	{
+		if ((*(t - 1) == c) && (*t++ != c))
+			*spl++ = '\0';
+		else if (*t != c)
+			*spl++ = *t++;
+		else
+			t++;
+	}
+	return (spl);
+}
+
+char		**ft_split(char const *s, char c)
 {
 	char	**spl;
 	char	*t;
 	size_t	t_len;
 	int		seq_c;
 
-	t = ft_strctrim(s, c);
+	if (!(t = ft_strctrim(s, c)))
+		return (NULL);
 	t_len = ft_strlen(t);
 	while (*t)
 		seq_c = ((*t == c) && (*++t == c)) ? ++seq_c : seq_c;
-	if (!(spl = (char **)malloc(t_len - seq_c + 1)))
-		return (NULL);
-	t -= t_len;
-	while (*t)
+	if (!(spl = (char **)malloc((sizeof(char *)) * (t_len - seq_c + 1))))
 	{
-		if ((*(t - 1) == c) && (*t++ != c))
-			**spl++ = '\0';
-		else if (*t != c)
-			**spl++ = *t++;
-		else
-			t++;
+		free(t);
+		return (NULL);
 	}
-	**spl = (NULL);
+	t -= t_len;
+	spl = t;
+	t = ft_subsplit(spl, t, c);
+	*t = (NULL);
 	return (spl);
 }
