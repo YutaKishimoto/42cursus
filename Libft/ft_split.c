1633dc3ed28011056b6c340129f3ba79c1f5af19
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-28 00:16:27 +0900

    Re:Re:WRITE F****** SPLIT YEAH

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 19:45:53 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/27 06:03:32 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/28 00:15:24 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,95 +39,80 @@ static size_t	ft_spltlen(const char *s, int c)
 	return (len);
 }
 
-static char		*ft_subsplt(char const *s, int len)
+static void		ft_spltfree(char **c, int i)
 {
-	char	*spl;
-	int		i;
-
-	if (!(spl = (char *)malloc(sizeof(char) * len + 1)))
-		return (NULL);
-	i = 0;
-	while (len-- > 0)
-	{
-		spl[i] = s[i];
-		i++;
-	}
-	spl[i] = '\0';
-	return (spl);
-}
-
-static char		**ft_splalloc(char **spl, char const *s, size_t len, char c)
-{
-	int		i;
-	int		ix;
-	size_t	iy;
-
-	i = 0;
-	ix = 0;
-	iy = 0;
-	while (s[i] && iy < len)
-	{
-		while (s[i] == c)
-			i++;
-		ix = i;
-		while (s[i] != c)
-			i++;
-		if (!(spl[iy++] = ft_subsplt(s + ix, i - ix)))
-		{
-			free(spl);
-			return (NULL);
-		}
-	}
-	spl[iy] = (NULL);
-	return (spl);
+	while (i >= 0)
+		free(c[i--]);
+	free(c);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	char	**spl;
-	size_t	len;
+	char	**sp;
 	int		i;
 	int		j;
 
-	len = ft_spltlen(s, c) + 1;
-	if (!(*s) || !(spl = (char **)malloc(sizeof(char *) * /* numOfIdxs */)))
+	if (!(*s) || !(sp = (char **)malloc(1)))
+		return (NULL);
+	if (!(*sp = (char *)malloc((ft_spltlen(s, c) + 1))))
 		return (NULL);
 	while (*s && (*s == c))
 		s++;
 	i = 0;
-	j = 0;
 	while (*s)
 	{
-		if (*s != c && *(s - 1) == c)
-		{
-			i++;
-			if (!(spl[i] = (char *)malloc(sizeof(char) * /* numOfChars*/)))
-			{
-				free(spl);
-				while (i-- != 0)
-					free(spl[i]);
-				return (NULL);
-			}
-		}
-		spl[i][j] = '\0';
+		j = 0;
+		while (*s && *s != c)
+			sp[i][j++] = *s++;
+		sp[i++][j] = '\0';
+		if (!(sp[i] = (char *)malloc(1)))
+			ft_spltfree(sp, i - 1);
+		while (*s && *s == c)
+			s++;
 	}
-	spl[i] = NULL;
-	return (spl);
+	sp[i] = NULL;
+	return (sp);
 }
 
-int main(void){
+int				main(void)
+{
 	int i;
 
-	char *s1 = "a,b,c";
+	char *s1 = ",,a,bc,,cde,,,defg,e,,,fgh,,,,";
 	char c = ',';
 	char **r1 = ft_split(s1, c);
 
 	i = 0;
 	printf("split\"%s\"\n\n", s1);
-	while (r1)
+	while (r1[i])
 	{
 		printf("r1[%d] : \"%s\"\n", i, r1[i]);
 		i++;
 	}
+
+	char *s2 = "aaa,,,,,,,,aaaaa";
+	char c2 = ',';
+	char **r2 = ft_split(s2, c2);
+
+	i = 0;
+	printf("split\"%s\"\n\n", s2);
+	while (r2[i])
+	{
+		printf("r2[%d] : \"%s\"\n", i, r2[i]);
+		i++;
+	}
+
+	char *s3 = ",,,,,,,,,   abcd,,ab,,cd,,,efg,,,hij,";
+	char c3 = ',';
+	char **r3 = ft_split(s3, c3);
+
+	i = 0;
+	printf("split\"%s\"\n\n", s3);
+	while (r3[i])
+	{
+		printf("r1[%d] : \"%s\"\n", i, r3[i]);
+		i++;
+	}
+
 	return (0);
 }
