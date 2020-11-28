b342f951c0816f34547670bc8173332477724637
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-11-28 20:58:30 +0900

    1st-fix Libft

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 19:45:53 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/11/28 04:18:59 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/11/28 20:49:02 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 #include <stdlib.h>
 
 /*
-** delete INCLUDE <stdio> and MAIN FUNC!!!!
+** #include <stdio.h>
 */
 
-#include <stdio.h>
+static size_t	ft_splt_idx(const char *s, int c)
+{
+	size_t	idx;
 
-static size_t	ft_spltlen(const char *s, int c)
+	idx = 0;
+	while (*s)
+	{
+		if (*s != c && (*(s + 1) == c || *(s + 1) == '\0'))
+			idx++;
+		s++;
+	}
+	idx++;
+	return (idx);
+}
+
+static size_t	ft_splt_len(const char *s, int c)
 {
 	size_t	len;
 	char	*end_s;
@@ -30,8 +43,8 @@ static size_t	ft_spltlen(const char *s, int c)
 	len = ft_strlen(s);
 	while (*(s + len - 1) == c && len > 0)
 		len--;
-	end_s = (char *)(s + len + 1);
-	while (s++ != end_s)
+	end_s = (char *)(s + len - 1);
+	while (s++ <= end_s)
 	{
 		if (*(s - 1) == c && *s == c)
 			len--;
@@ -39,14 +52,7 @@ static size_t	ft_spltlen(const char *s, int c)
 	return (len);
 }
 
-static void		ft_spltfree(char **c, int i)
-{
-	while (i >= 0)
-		free(c[i--]);
-	free(c);
-}
-
-static void		ft_spltcpy(char **sp, char const *s, char c )
+static void		ft_splt_cpy(char **sp, char const *s, char c)
 {
 	int i;
 	int j;
@@ -57,13 +63,14 @@ static void		ft_spltcpy(char **sp, char const *s, char c )
 		j = 0;
 		while (*s && *s == c)
 			s++;
-		while (*s && (*s != c))
+		while (*s && *s != c)
+		{
 			sp[i][j++] = *s++;
-		if (*s)
+		}
+		if (*s || j != 0)
 		{
 			sp[i++][j] = '\0';
-			if (!(sp[i] = (char *)malloc(1)))
-				ft_spltfree(sp, i);
+			sp[i] = &sp[i - 1][j + 1];
 		}
 	}
 	sp[i] = NULL;
@@ -73,90 +80,97 @@ char			**ft_split(char const *s, char c)
 {
 	char	**sp;
 
-	if (!*s || !(sp = (char **)malloc(sizeof(char *))))
+	if (!*s || !(sp = (char **)malloc(sizeof(char *) * ft_splt_idx(s, c))))
 		return (NULL);
-	if (!(*sp = (char *)malloc(ft_spltlen(s, c) + 1)))
+	if (!(*sp = (char *)malloc(ft_splt_len(s, c) + 2)))
+	{
+		free(sp);
 		return (NULL);
-	ft_spltcpy(sp, s, c);
+	}
+	ft_splt_cpy(sp, s, c);
 	return (sp);
 }
 
-int				main(void)
-{
-	int i;
-	int j;
-
-	char *s1 = ",,a,bc,,cde,,,defg,e,,,fgh,,,,";
-	char c = ',';
-	char **r1 = ft_split(s1, c);
-
-	i = 0;
-	printf("----\nsplit : \"%s\"\n\n", s1);
-	while (r1[i])
-	{
-		printf("r1[%d] : \"%s\"\t%12p\n", i, r1[i], r1[i]);
-		i++;
-	}
-	i = 0;
-	j = 0;
-	while (r1[i])
-	{
-		j = 0;
-		while (r1[i][j])
-		{
-			printf("r1[%d][%d] : %c : %12s\n", i, j, r1[i][j], &r1[i][j]);
-			j++;
-		}
-		i++;
-	}
-
-	char *s2 = "aaa,,,,,,,,aaaaa";
-	char c2 = ',';
-	char **r2 = ft_split(s2, c2);
-
-	i = 0;
-	printf("----\nsplit : \"%s\"\n\n", s2);
-	while (r2[i])
-	{
-		printf("r2[%d] : \"%s\"\t%12p\n", i, r2[i], r2[i]);
-		i++;
-	}
-	i = 0;
-	j = 0;
-	while (r2[i])
-	{
-		j = 0;
-		while (r2[i][j])
-		{
-			printf("r2[%d][%d] : %c : %12s\n", i, j, r2[i][j], &r2[i][j]);
-			j++;
-		}
-		i++;
-	}
-
-	char *s3 = ",,,,,,,,,   abcd,,ab,,cd,,,efg,,,hij,";
-	char c3 = ',';
-	char **r3 = ft_split(s3, c3);
-
-	i = 0;
-	printf("----\nsplit : \"%s\"\n\n", s3);
-	while (r3[i])
-	{
-		printf("r3[%d] : \"%s\"\t%12p\n", i, r3[i], r3[i]);
-		i++;
-	}
-	i = 0;
-	j = 0;
-	while (r3[i])
-	{
-		j = 0;
-		while (r3[i][j])
-		{
-			printf("r3[%d][%d] : %c : %12s\n", i, j, r3[i][j], &r3[i][j]);
-			j++;
-		}
-		i++;
-	}
-
-	return (0);
-}
+/*
+** testcase
+**
+** int				main(void)
+** {
+** 	int i;
+** 	int j;
+**
+** 	char *s1 = "split  ||this|for|me|||||!|";
+** 	char c = '|';
+** 	char **r1 = ft_split(s1, c);
+**
+** 	i = 0;
+** 	printf("----\nsplit : \"%s\"\n\n", s1);
+** 	while (r1[i])
+** 	{
+** 		printf("r1[%d] : \"%s\"\t%12p\n", i, r1[i], r1[i]);
+**  		i++;
+** 	}
+** 	printf("r1[%d] : \"%s\"\t%12p\n", i, r1[i], r1[i]);
+** 	i = 0;
+** 	while (r1[i])
+** 	{
+** 		j = 0;
+** 		while (r1[i][j])
+** 		{
+** 			printf("r1[%d][%d] : %c : %12s\n", i, j, r1[i][j], &(r1[i][j]));
+** 			j++;
+** 		}
+** 		i++;
+** 	}
+**
+** 	char *s2 = "aaa,,,,,,,,aaaaa";
+** 	char c2 = ',';
+** 	char **r2 = ft_split(s2, c2);
+**
+** 	i = 0;
+** 	printf("----\nsplit : \"%s\"\n\n", s2);
+** 	while (r2[i])
+** 	{
+** 		printf("r2[%d] : \"%s\"\t%12p\n", i, r2[i], r2[i]);
+** 		i++;
+** 	}
+** 	printf("r2[%d] : \"%s\"\t%12p\n", i, r2[i], r2[i]);
+** 	i = 0;
+** 	while (r2[i])
+** 	{
+** 		j = 0;
+** 		while (r2[i][j])
+** 		{
+** 			printf("r2[%d][%d] : %c : %12s\n", i, j, r2[i][j], &(r2[i][j]));
+** 			j++;
+** 		}
+** 		i++;
+** 	}
+**
+** 	char *s3 = ",,,,,";
+** 	char c3 = ',';
+** 	char **r3 = ft_split(s3, c3);
+**
+** 	i = 0;
+** 	printf("----\nsplit : \"%s\"\n\n", s3);
+** 	while (r3[i])
+** 	{
+** 		printf("r3[%d] : \"%s\"\t%12p\n", i, r3[i], r3[i]);
+** 		i++;
+** 	}
+** 	printf("r3[%d] : \"%s\"\t%12p\n", i, r3[i], r3[i]);
+** 	i = 0;
+** 	while (r3[i])
+** 	{
+** 		j = 0;
+** 		while (r3[i][j])
+** 		{
+** 			printf("r3[%d][%d] : %c : %12s\n", i, j, r3[i][j], &r3[i][j]);
+** 			j++;
+** 		}
+** 		i++;
+** 	}
+**
+** 	return (0);
+** }
+*/
