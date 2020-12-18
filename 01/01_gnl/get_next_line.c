2ebb3261aa05a82b7eca0f41191623d711219819
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-12-19 03:35:44 +0900

    add err conditions

--- 01/01_gnl/get_next_line.c
+++ 01/01_gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 08:59:55 by tyamagis          #+#    #+#             */
-/*   Updated: 2020/12/17 22:36:22 by tyamagis         ###   ########.fr       */
+/*   Updated: 2020/12/19 03:34:48 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@ int get_next_line(int fd, char **line)
 	size_t		len;
 	static int	i;
 
-	if (fd < 0 || fd > 255 || !(line))
+	if (fd < 0 || fd > 255 || !line || !(buf = malloc(BUFFER_SIZE)))
 		return (-1);
-	if (!(buf = (char *)malloc(BUFFER_SIZE)))
-		return (-1);
+	len = 0;
 	while (read(fd, buf, BUFFER_SIZE) > 0)
 	{
 		while (buf[i] != '\n' && i++ < BUFFER_SIZE)
 			len++;
-		if (buf[i] == '\n') // case 1 : \n exist in a 1st loop
+		if (buf[i] == '\n')
 		{
 			if (!(*line = (char *)malloc(len + 1)))
 			{
@@ -41,10 +40,7 @@ int get_next_line(int fd, char **line)
 			*line[i] = '\0';
 			return (1);
 		}
-		else // case 2 : \n dont exist in 1st loop
-		{
-			
-		}
+		// case 2 : \n dont exist in 1st loop
 		// case 3 : EOF(ctrl + D) is in loop
 		// case 4 : empty
 	}
