da0f3be5c324a0046b9f23a726dc13aeb461a943
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-12-27 15:08:20 +0900

    add 99/node/ex02

--- 99_reloaded/hello_node/ex02/sum_args.js
+++ 99_reloaded/hello_node/ex02/sum_args.js
@@ -6,19 +6,26 @@
 //   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2020/12/24 07:29:59 by tyamagis          #+#    #+#             //
-//   Updated: 2020/12/24 08:54:24 by tyamagis         ###   ########.fr       //
+//   Updated: 2020/12/24 10:33:04 by tyamagis         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 var i = 2;
 var sum = 0;
 
-while (i < process.argv.length){
-	sum += +(process.argv[i]);
-	i++;
+if (process.argv.length == 2)
+{
+	console.log(" ! -- Error -- ! ");
+	console.log("no input. Retry >>");
+} else {
+	while (i < process.argv.length){
+		sum += +(process.argv[i]);
+		i++;
+	}
+	if (!sum){
+		console.log(" ! -- Error -- ! ");
+		console.log("Your input includes non-NUMERIC character.");
+		console.log("Result may be 'NaN'. Retry >>");
+	}
+	console.log(sum);
 }
-if (!sum){
-	console.log("Your input includes non-NUMERIC character.");
-	console.log("Result may be 'NaN'. Retry >>");
-}
-console.log(sum);
