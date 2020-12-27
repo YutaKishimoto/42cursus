d59aed2db52046ebebdc32cf8fb314ea29897670
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2020-12-27 21:07:44 +0900

    mod err msg

--- 99_reloaded/hello_node/ex04/asyncio.js
+++ 99_reloaded/hello_node/ex04/asyncio.js
@@ -6,13 +6,13 @@
 //   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2020/12/27 15:14:16 by tyamagis          #+#    #+#             //
-//   Updated: 2020/12/27 18:14:24 by tyamagis         ###   ########.fr       //
+//   Updated: 2020/12/27 20:29:20 by tyamagis         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 if (process.argv[2] == undefined)
 {
-	console.log("No Args, RETRY !!");
+	console.log("No Args, RETRY >>");
 	return ;
 }
 require("fs").readFile(process.argv[2], (err, data) => {
