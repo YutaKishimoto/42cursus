172c1f7a5864bf4bcdbff8dd53f8f7de099d64db
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2021-01-11 18:43:14 +0900

    mod 99/node/ex07 recursively calling

--- 99_reloaded/hello_node/ex07/async-http-collect.js
+++ 99_reloaded/hello_node/ex07/async-http-collect.js
@@ -6,8 +6,36 @@
 //   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2021/01/01 21:22:27 by tyamagis          #+#    #+#             //
-//   Updated: 2021/01/06 09:18:44 by tyamagis         ###   ########.fr       //
+//   Updated: 2021/01/11 18:41:16 by tyamagis         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
-// DAMEDESU
+let i = 2;
+
+function put_html(url){
+	try {
+		var html = "";
+		var req = require("http").get(url, (res) => {
+			res.on("data", (chunk) => {
+				html += chunk.toString();
+			});
+			res.on("end", () => {
+				console.log(html);
+				if (++i < 5){
+					put_html(process.argv[i]);
+				}
+			});
+		});
+		req.on("error", (e) => {
+			console.log(e.message + "\nRETRY >>");
+		});
+	} catch (e){
+		console.log(e.message + "\nRETRY >>");
+	}
+}
+
+if (process.argv.length !== 5){
+	console.log("ARGS Err:\n3 [http url] needed.\nRETRY >>");
+	return ;
+}
+put_html(process.argv[i]);
