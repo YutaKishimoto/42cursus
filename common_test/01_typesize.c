32ae6630dd9eab0da3feebebea53796fc4738383
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2021-02-07 16:04:50 +0900

    add 01 long double

--- common_test/01_typesize.c
+++ common_test/01_typesize.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 15:13:57 by tyamagis          #+#    #+#             */
-/*   Updated: 2021/02/07 15:50:36 by tyamagis         ###   ########.fr       */
+/*   Updated: 2021/02/07 15:58:52 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,25 @@ int main(void)
 	printf("CHAR_min =\t%d\nCHAR_MAX =\t%d\n\n", CHAR_MIN, CHAR_MAX);
 
 	printf("[short] is\t%lu Byte.\n", sizeof(short));
-	printf("SHRT_min =\t%d\n, SHRT_MAX =\t%d\n\n", SHRT_MIN, SHRT_MAX);
+	printf("SHRT_min =\t%d\nSHRT_MAX =\t%d\n\n", SHRT_MIN, SHRT_MAX);
 
 	printf("[int] is\t%lu Byte.\n", sizeof(int));
-	printf("INT_min =\t%d, INT_MAX =\t%d\n\n", INT_MIN, INT_MAX);
+	printf("INT_min =\t%d\nINT_MAX =\t%d\n\n", INT_MIN, INT_MAX);
 
 	printf("[long] is\t%lu Byte.\n", sizeof(long));
-	printf("LONG_min =\t%ld, LONG_MAX =\t%ld\n\n", LONG_MIN, LONG_MAX);
+	printf("LONG_min =\t%ld\nLONG_MAX =\t%ld\n\n", LONG_MIN, LONG_MAX);
 
 	printf("[long long] is\t%lu Byte.\n", sizeof(long long));
 	printf("long long is the same as long.\n");
 
 	printf("[float] is\t%lu Byte.\n", sizeof(float));
-	printf("float_min =\t%f, float_MAX =\t%f\n\n", FLT_MIN, FLT_MAX);
+	printf("float_min =\t%f\nfloat_MAX =\t%f\n\n", FLT_MIN, FLT_MAX);
 
 	printf("[double] is\t%lu Byte.\n", sizeof(double));
-	printf("double_min =\t%lf, double_MAX =\t%lf\n\n", DBL_MIN, DBL_MAX);
+	printf("double_min =\t%lf\ndouble_MAX =\t%lf\n\n", DBL_MIN, DBL_MAX);
 
 	printf("[long double] is\t%lu Byte.\n\n", sizeof(long double));
-	printf("long double_min =\t%Lf, long double_MAX =\t%Lf\n\n", LDBL_MIN, LDBL_MAX);
+	printf("long double_min =\t%Lf\nlong double_MAX =\t%Lf\n\n", LDBL_MIN, LDBL_MAX);
 
 	printf("[pointer] is \t%lu Byte.\n", sizeof(int*));
 }
