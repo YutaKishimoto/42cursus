6a116dcd247f75101fee861207fa46788def7490
YutaKishimoto@GCML <66864978+YutaKishimoto@users.noreply.github.com>
2021-02-15 23:15:58 +0900

    NC - add #defines oflags

--- 99_common_test/00_stdlibs/fcntl_h/open.c
+++ 99_common_test/00_stdlibs/fcntl_h/open.c
@@ -6,7 +6,7 @@
 /*   By: tyamagis <tyamagis@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 22:48:28 by tyamagis          #+#    #+#             */
-/*   Updated: 2021/02/15 23:00:12 by tyamagis         ###   ########.fr       */
+/*   Updated: 2021/02/15 23:15:25 by tyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
 ** - RETURN VALUE -
 ** Success --> non-negative int termed a fd.
 ** Fail --> return -1 and set errno.
+** - OFLAGS (a part of)
+** #define O_RDONLY		0 : read only
+** #define O_WRONLY		1 : write only
+** #define O_RDWR		2 : read and write only
+** #define O_ACCMODE	3 : mask for above modes (??
 */
 
 #include <fcntl.h>
 
 int main(){
-	int fd = open(/* path, oflag */);
+	int fd = open(/* char *path, int oflag */);
 	// read();
 	// write(); // or other func to use opened file.
 	// close();
